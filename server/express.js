17abdaddada2718bbb57421ab8a1ca3ee0818173
maihnguyen0221 <maihnguyen0221@gmail.com>
2025-05-03 19:05:58 -0600

    revert express file

--- server/express.js
+++ server/express.js
@@ -26,9 +26,8 @@ import devBundle from './devBundle'
 const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
 
-if (process.env.NODE_ENV !== 'production') {
-  devBundle.compile(app)
-}
+//comment out before building for production
+devBundle.compile(app)
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json())
